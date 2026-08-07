a0702f00d516f11e3bc2401dba73cab6b81b4130
Delusions6515 <213381333+Delusions6515@users.noreply.github.com>
2026-08-07 16:26:29 +0800

    chore: initialize vue webui project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <main class="page">
+    <h1>Sub-Store Module</h1>
+
+    <p>Miuix Vue WebUI</p>
+  </main>
+</template>
+
+<style scoped>
+.page {
+  padding: 24px;
+}
+</style>
